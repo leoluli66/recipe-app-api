0cfcc7695ddacbb78ddfa75c0543d372435a856e
Li Lu <llu85@gatech.edu>
2021-11-07 22:52:48 -0800

    Configure postgres db

--- app/app/settings.py
+++ app/app/settings.py
@@ -73,11 +73,15 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
+# benefit: we don't need to modify here if running our app on different servers
+# we just need to change the environment variables in the docker-compose
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
 
